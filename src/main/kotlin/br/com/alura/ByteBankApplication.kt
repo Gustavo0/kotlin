85afb9fa732772cba1767176c5ca5eba264f9395
Gustavo Gomes <gustavo.sgomess@gmail.com>
2021-09-21 07:10:32 -0300

    Funções anônimas e lambdas com parametro

--- src/main/kotlin/br/com/alura/ByteBankApplication.kt
+++ src/main/kotlin/br/com/alura/ByteBankApplication.kt
@@ -8,4 +8,33 @@ class ByteBankApplication
 
 fun main(args: Array<String>) {
 	runApplication<ByteBankApplication>(*args)
+//	testaTipoFuncaoReferencia()
+//	testaTipoFuncaoClasse()
+	val minhaFuncaoLambda = { a: Int, b: Int ->
+		a + b
+	}
+	println(minhaFuncaoLambda(15, 10))
+
+	val minhaFuncaoAnonima = fun(a: Int, b: Int) : Int {
+		return a + b
+	}
+	println(minhaFuncaoAnonima(20, 10))
+}
+
+fun testaTipoFuncaoClasse() {
+	val minhaFuncaoClasse: (Int, Int) -> Int = Soma()
+	println(minhaFuncaoClasse(10, 10))
+}
+
+fun testaTipoFuncaoReferencia() {
+	val minhaFuncao: (Int, Int) -> Int = ::soma
+	println(minhaFuncao(5, 10))
+}
+
+fun soma(a: Int, b: Int) : Int {
+	return a + b
+}
+
+class Soma : (Int, Int) -> Int {
+	override fun invoke(a: Int, b: Int): Int = a + b
 }
