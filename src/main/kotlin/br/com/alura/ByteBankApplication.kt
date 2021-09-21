4a7993caa6435b2ca3f8c4cd75395d1c6e5ac488
Gustavo Gomes <gustavo.sgomess@gmail.com>
2021-09-21 07:23:35 -0300

    High Order Function

--- src/main/kotlin/br/com/alura/ByteBankApplication.kt
+++ src/main/kotlin/br/com/alura/ByteBankApplication.kt
@@ -1,5 +1,6 @@
 package br.com.alura
 
+import br.com.alura.bytebank.modelo.Endereco
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,49 +9,17 @@ class ByteBankApplication
 
 fun main(args: Array<String>) {
 	runApplication<ByteBankApplication>(*args)
-//	testaTipoFuncaoReferencia()
-//	testaTipoFuncaoClasse()
-	val minhaFuncaoLambda = { a: Int, b: Int ->
-		a + b
+	val testeFuncao: () -> Unit
+	Endereco().let {
+		it
 	}
-	println(minhaFuncaoLambda(15, 10))
-
-	val minhaFuncaoAnonima = fun(a: Int, b: Int) : Int {
-		return a + b
-	}
-	println(minhaFuncaoAnonima(20, 10))
-
-	val calculaBonificacao: (salario: Double) -> Double = lambda@{ salario ->
-		if(salario > 1000.0){
-			return@lambda salario + 50
-		}
-		salario + 100.0
-	}
-	println(calculaBonificacao(1000.0))
-
-	val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double {
-		if(salario > 1000.0){
-			return salario + 50
-		}
-		return salario + 100.0
-	}
-	println(calculaBonificacaoAnonima(1100.0))
+	"".let(::testeRecebeString)
+	1.let {
+		it}
+	teste (1, {})
 }
 
-fun testaTipoFuncaoClasse() {
-	val minhaFuncaoClasse: (Int, Int) -> Int = Soma()
-	println(minhaFuncaoClasse(10, 10))
-}
+fun testeRecebeString(valor: String){}
 
-fun testaTipoFuncaoReferencia() {
-	val minhaFuncao: (Int, Int) -> Int = ::soma
-	println(minhaFuncao(5, 10))
-}
-
-fun soma(a: Int, b: Int) : Int {
-	return a + b
-}
-
-class Soma : (Int, Int) -> Int {
-	override fun invoke(a: Int, b: Int): Int = a + b
-}
+//higher order function
+fun teste(teste: Int, bloco: () -> Unit) {}
