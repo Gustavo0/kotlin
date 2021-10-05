5f16fa04db7a8ec5a0ed1eb7fdf3ec722a7db33f
Gustavo Gomes <gustavo.sgomess@gmail.com>
2021-10-05 07:57:43 -0300

    Testa run e with

--- src/main/kotlin/br/com/alura/ByteBankApplication.kt
+++ src/main/kotlin/br/com/alura/ByteBankApplication.kt
@@ -1,8 +1,5 @@
 package br.com.alura
 
-import br.com.alura.bytebank.modelo.Autenticavel
-import br.com.alura.bytebank.modelo.Endereco
-import br.com.alura.bytebank.modelo.SistemaInterno
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -12,5 +9,5 @@ class ByteBankApplication
 fun main(args: Array<String>) {
 	runApplication<ByteBankApplication>(*args)
 
-	
+
 }
